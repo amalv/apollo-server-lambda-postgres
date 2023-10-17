82ea575e8991500cd06a1bb956c8d16bbadffd81
amalv <1252707+amalv@users.noreply.github.com>
2023-10-18 00:05:23 +0200

    feat: initial commit
    This commit sets up a new Apollo Server GraphQL API that is deployed to AWS Lambda. The following changes were made:
    
    - Added dependencies for Apollo Server, Postgres, and AWS Lambda
    - Created a server.ts file to define the GraphQL server and schema
    - Added a serverless.yml file to define the AWS Lambda function configuration
    - Added a query.json file to define sample GraphQL queries

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import { ApolloServer } from '@apollo/server';
+import { startServerAndCreateLambdaHandler, handlers } from '@as-integrations/aws-lambda';
+
+// The GraphQL schema
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
+
+// Set up Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// This final export is important!
+export const graphqlHandler = startServerAndCreateLambdaHandler(
+  server,
+  // We will be using the Proxy V2 handler
+  handlers.createAPIGatewayProxyEventV2RequestHandler(),
+);
