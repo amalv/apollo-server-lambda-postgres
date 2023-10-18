ce83c0fd568e7fcf34a36e6ef2641213140b3d34
amalv <1252707+amalv@users.noreply.github.com>
2023-10-18 20:53:24 +0200

    feat: add AWS RDS PostgreSQL connection and certs
    This commit adds handling for SSL certificates in the `server.ts` file and updates the project configuration in the `serverless.yml` file.
    
    The `server.ts` file now reads the SSL certificate from the file system based on the `NODE_ENV` and `DB_SSL_CERT` environment variables. It also creates a new PostgreSQL client and connects to the database usin the environment variables. The `.env_sample` file now contains the necessary environment variables for the database connection.
    
    The `serverless.yml` file now sets the `NODE_ENV` environment variable to `production` and includes the `certs` directory in the deployment package.

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# 1.0.0 (2023-10-17)
+
+
+### Features
+
+* add support for Semantic Release ([b652550](https://github.com/amalv/apollo-server-lambda-postgres/commit/b65255041cf20450df1182755d279a466efbf319))
+* initial commit ([bf9e353](https://github.com/amalv/apollo-server-lambda-postgres/commit/bf9e353cde233be0e39879af656d3959bec9299a))
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,49 @@
-import { ApolloServer } from '@apollo/server';
-import { startServerAndCreateLambdaHandler, handlers } from '@as-integrations/aws-lambda';
+import { ApolloServer } from "@apollo/server";
+import {
+  startServerAndCreateLambdaHandler,
+  handlers,
+} from "@as-integrations/aws-lambda";
+import { config } from "dotenv";
+import { Client } from "pg";
+import * as fs from "fs";
+
+if (process.env.NODE_ENV !== "production") {
+  config();
+}
+
+let sslCertificate;
+
+if (process.env.NODE_ENV === "production") {
+  sslCertificate = fs.readFileSync("/var/task/certs/global-bundle.pem");
+} else if (process.env.DB_SSL_CERT) {
+  sslCertificate = fs.readFileSync(process.env.DB_SSL_CERT);
+}
+
+if (process.env.DB_SSL_CERT) {
+  sslCertificate = fs.readFileSync(process.env.DB_SSL_CERT);
+}
+
+const dbPort = process.env.DB_PORT;
+
+if (!dbPort || isNaN(parseInt(dbPort))) {
+  throw new Error("Invalid DB port");
+}
+
+// Create a new PostgreSQL client
+const client = new Client({
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_ENDPOINT,
+  database: process.env.DB_NAME,
+  port: dbPort,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: sslCertificate || undefined,
+  },
+});
+
+// Connect to the database
+client.connect();
 
 // The GraphQL schema
 const typeDefs = `#graphql
@@ -8,10 +52,20 @@ const typeDefs = `#graphql
   }
 `;
 
-// A map of functions which return data for the schema.
+// A resolver for the hello query
+
 const resolvers = {
   Query: {
-    hello: () => 'world',
+    hello: async () => {
+      const result = await client.query("SELECT version(), current_database()");
+
+      await client.end();
+
+      const version = result.rows[0].version;
+      const databaseName = result.rows[0].current_database;
+
+      return `Connected to ${databaseName} (version ${version})`;
+    },
   },
 };
 
@@ -25,5 +79,5 @@ const server = new ApolloServer({
 export const graphqlHandler = startServerAndCreateLambdaHandler(
   server,
   // We will be using the Proxy V2 handler
-  handlers.createAPIGatewayProxyEventV2RequestHandler(),
+  handlers.createAPIGatewayProxyEventV2RequestHandler()
 );
