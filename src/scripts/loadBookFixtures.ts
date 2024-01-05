ac67b61de5cf12292e133bbc1e5df0bf9b4efc33
amalv <1252707+amalv@users.noreply.github.com>
2024-01-05 19:36:59 +0100

    refactor: remove TypeORM and related code in favor of Drizzle

--- src/scripts/loadBookFixtures.ts
+++ src/scripts/loadBookFixtures.ts
@@ -1,8 +1,6 @@
+import { client, db } from "../db";
 import { faker } from "@faker-js/faker";
-import { FixtureDataSource } from "../data-source";
-import { User } from "../entity/User";
-import { Book } from "../entity/Book";
-import { Favorite } from "../entity/Favorite";
+import { book, favorite } from "../schema";
 import booksData from "../fixtures/raw/books.json";
 
 const initializeBooks = async (bookData) => {
@@ -12,45 +10,28 @@ const initializeBooks = async (bookData) => {
     );
     return;
   }
-  const book = new Book();
-  book.title = bookData.title;
-  book.author = bookData.author;
-  book.publicationDate = new Date(bookData.publicationDate);
-  book.image = bookData.image || faker.image.url({ width: 150, height: 150 });
-  book.rating = bookData.rating;
-  book.ratingsCount = bookData.ratingsCount;
-  return FixtureDataSource.manager.save(book);
+
+  const bookValues = {
+    title: bookData.title,
+    author: bookData.author,
+    publicationDate: new Date(bookData.publicationDate).toISOString(),
+    image: bookData.image || faker.image.url({ width: 150, height: 150 }),
+    rating: bookData.rating,
+    ratingsCount: bookData.ratingsCount,
+  };
+
+  return db.insert(book).values(bookValues).returning();
 };
 
-const initializeUsers = async () => {
-  const user = new User();
-  user.auth0Id = "1234567890";
-  return FixtureDataSource.manager.save(user);
-};
+(async () => {
+  console.log("Truncating the favorite and book table...");
+  await db.delete(favorite);
+  await db.delete(book);
 
-const initializeFavorites = async (user, book) => {
-  const favorite = new Favorite();
-  favorite.userId = user.id;
-  favorite.bookId = book.id;
-  return FixtureDataSource.manager.save(favorite);
-};
-
-FixtureDataSource.initialize()
-  .then(async () => {
-    console.log("Dropping and recreating the tables...");
-    await FixtureDataSource.manager.query('TRUNCATE "favorite" CASCADE');
-    await FixtureDataSource.manager.query('TRUNCATE "user", "book" CASCADE');
-    await FixtureDataSource.synchronize(true);
-
-    console.log("Inserting new books into the database...");
-    const books = await Promise.all(booksData.map(initializeBooks));
-
-    console.log("Inserting new users into the database...");
-    const user = await initializeUsers();
-
-    console.log("Marking some books as favorites for the user...");
-    await Promise.all(
-      books.slice(0, 5).map((book) => initializeFavorites(user, book))
-    );
-  })
-  .catch((error) => console.log(error));
+  console.log("Inserting new books into the database...");
+  const books = await Promise.all(
+    booksData.map((bookData) => initializeBooks(bookData))
+  );
+  console.log(`Inserted ${books.length} books.`);
+  await client.end();
+})().catch(console.error);
